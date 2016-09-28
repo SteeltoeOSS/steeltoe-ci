0fb941d4c807b303559f0df02f0198952ad55918
Dave Goddard <dave@goddard.id.au>
2016-09-28 12:01:11 -0400

    delete service before purge on space deletion
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- scripts/delete-space.sh
+++ scripts/delete-space.sh
@@ -3,6 +3,9 @@ set -ex
 
 ./cf-space/login
 
+# Deleet all services in space
+cf services | sed '1,4d' | cut -d ' ' -f 1 | xargs -n1 cf delete-service -f
+
 # Purge all services in space
 cf services | sed '1,4d' | cut -d ' ' -f 1 | xargs -n1 cf purge-service-instance -f
 
