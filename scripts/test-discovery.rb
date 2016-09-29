1bf170896f230039de48a97a8eb042d49335ebc2
Dave Goddard <dave@goddard.id.au>
2016-09-29 14:49:48 -0400

    Improve discovery test output
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- scripts/test-discovery.rb
+++ scripts/test-discovery.rb
@@ -5,8 +5,12 @@ require 'json'
 
 
 space = ENV['SPACE'] or raise 'Please specify space'
-json = JSON.parse(Net::HTTP.get("fortuneui-#{space}.pcfdev.shoetree.io", '/random'))
+puts "Check http://fortuneui-#{space}.pcfdev.shoetree.io/random"
 # => {"id"=>1021, "text"=>"The greatest risk is not taking one."}
+body Net::HTTP.get("fortuneui-#{space}.pcfdev.shoetree.io", '/random')
+json = JSON.parse(body)
 
 json['id'].to_i > 0 or raise "JSON did not return an id"
 json['text'].to_s.length > 0 or raise "JSON did not return text"
+
+puts "Success"
