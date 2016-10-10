544400d8307956f8dc6285630b9d2cfbbf885cdd
Dave Goddard <dave@goddard.id.au>
2016-10-10 11:54:08 -0400

    fixup

--- scripts/test-security.rb
+++ scripts/test-security.rb
@@ -2,32 +2,26 @@
 require 'rubygems'
 require 'mechanize'
 require 'json'
-require 'logger'
 
 space = ENV['SPACE'] or raise 'Please specify space'
 agent = Mechanize.new
-agent.log = Logger.new(STDERR)
 agent.verify_mode = 0
 
-begin
-  page = agent.get("https://single-signon-#{space}.cfapps.pez.pivotal.io/Home/About")
-  page.uri.to_s.match('https://myuaa-.*.cfapps.pez.pivotal.io/login') or raise 'Should force Auth'
-  page = page.form_with(action: '/login.do') do |f|
-    f.username = "user#{space}"
-    f.password = 'Password1!'
-  end.submit
-  page.uri.to_s.match("https://single-signon-#{space}.cfapps.pez.pivotal.io/Home/About") or raise "Should have redirected to About"
-  page.body.match('Your About page') or raise 'Should now allow testgroup access'
+page = agent.get("https://single-signon-#{space}.cfapps.pez.pivotal.io/Home/About")
+page.uri.to_s.match('https://myuaa-.*.cfapps.pez.pivotal.io/login') or raise 'Should force Auth'
+page = page.form_with(action: '/login.do') do |f|
+  f.username = "user#{space}"
+  f.password = 'Password1!'
+end.submit
+page.uri.to_s.match("https://single-signon-#{space}.cfapps.pez.pivotal.io/Home/About") or raise "Should have redirected to About"
+page.body.match('Your About page') or raise 'Should now allow testgroup access'
 
-  page = page.link_with(:href => /Contact/).click
-  page.body.match('Insufficient permissions') or raise 'Should not allow testgroup1 access'
+page = page.link_with(:href => /Contact/).click
+page.body.match('Insufficient permissions') or raise 'Should not allow testgroup1 access'
 
-  page = page.link_with(:href => /InvokeJwtSample/).click
-  page.body.match('Invoke Jwt Sample Application') or raise 'Should allow jwt access'
-  values = JSON.parse(page.search('h3').text)
-  values == ["value1","value2"] or raise 'Should display values from jwt api'
+page = page.link_with(:href => /InvokeJwtSample/).click
+page.body.match('Invoke Jwt Sample Application') or raise 'Should allow jwt access'
+values = JSON.parse(page.search('h3').text)
+values == ["value1","value2"] or raise 'Should display values from jwt api'
 
-  puts "Test Passed"
-rescue => e
-  p e
-end
+puts "Test Passed"
