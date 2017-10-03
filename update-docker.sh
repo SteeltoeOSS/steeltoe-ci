a66a8ea728213f00bdb47e6818a8c49ca221536f
Chris Cheetham <chris@cheetham.com>
2017-10-03 05:27:49 -0700

    Initial support for building docker images

---
+++ update-docker.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+basedir=$(dirname $0)
+for dfile in $basedir/dockerfiles/*/Dockerfile; do
+    name=$(basename $(dirname $dfile))
+    docker build -t steeltoeoss/$name $basedir/dockerfiles/$name || exit
+done
