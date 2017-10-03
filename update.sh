babf5dc45dec00ba11300abc57949a0c1cb67338
Chris Cheetham <chris@cheetham.com>
2017-10-03 14:49:31 -0400

    Don't assume pwd is same as project root

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-for file in pipelines/*.yml; do
+basedir=$(dirname $0)
+for file in $basedir/pipelines/*.yml; do
   base=$(basename $file .yml)
   echo $base
   fly -t steeltoe set-pipeline -l <(lpass show --notes 'Shared-Steeltoe/concourse.yml') -p $base -c $file
