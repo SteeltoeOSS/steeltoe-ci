2a5b9496caccf7eb134d8d14fc1a3b546152d271
Chris Cheetham <chris@cheetham.com>
2018-03-28 15:25:22 -0400

    Add tests for remaining configuration samples

--- jenkins/samples.groovy
+++ jenkins/samples.groovy
@@ -6,32 +6,43 @@ recipients = [
     'ccheetham',
 ]
 
-job('steeltoe-samples-configuration-simple') {
-    displayName('Steeltoe Samples : Configuration : Simple')
-    wrappers {
-        credentialsBinding {
-            usernamePassword('STEELTOE_PCF_CREDENTIALS', 'steeltoe-pcf')
+samples = [
+    'Configuration/src/AspDotNetCore/CloudFoundry',
+    'Configuration/src/AspDotNetCore/Simple',
+    'Configuration/src/AspDotNetCore/SimpleCloudFoundry',
+]
+
+def sample2Job(def sample) {
+    "steeltoe-samples-${sample.split('/').findAll { !(it in ['src']) }.collect { it.toLowerCase() }.join('-')}"
+}
+
+samples.each { sample ->
+    job(sample2Job(sample)) {
+        wrappers {
+            credentialsBinding {
+                usernamePassword('STEELTOE_PCF_CREDENTIALS', 'steeltoe-pcf')
+            }
+            preBuildCleanup()
         }
-        preBuildCleanup()
-    }
-    label('steeltoe')
-    scm {
-        git {
-            remote {
-                github('SteeltoeOSS/Samples', 'https')
-                branch('dev')
+        label('steeltoe')
+        scm {
+            git {
+                remote {
+                    github('SteeltoeOSS/Samples', 'https')
+                    branch('dev')
+                }
             }
         }
-    }
-    steps {
-        shell('ci/jenkins.sh Configuration/src/AspDotNetCore/Simple')
-    }
-    publishers {
-        archiveArtifacts('test.log')
-        mailer(recipients.collect { "${it}@pivotal.io" }.join(' '), true, false)
-    }
-    logRotator {
-        numToKeep(5)
+        steps {
+            shell("ci/jenkins.sh ${sample}")
+        }
+        publishers {
+            archiveArtifacts('test.log')
+            mailer(recipients.collect { "${it}@pivotal.io" }.join(' '), true, false)
+        }
+        logRotator {
+            numToKeep(5)
+        }
     }
 }
 
