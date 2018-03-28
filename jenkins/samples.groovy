9a3631dcd732054d286347d98b4cac348f8d53ee
Chris Cheetham <chris@cheetham.com>
2018-03-28 13:23:03 -0400

    Initial stub for Samples on Jenkins

---
+++ jenkins/samples.groovy
@@ -0,0 +1,33 @@
+/**
+ * Jenkins DSL for Steeltoe Samples
+ */
+
+job('steeltoe-samples') {
+    displayName('Steeltoe Samples')
+    wrappers {
+        credentialsBinding {
+            usernamePassword 'STEELTOE_PCF_CREDENTIALS', 'steeltoe-pcf'
+        }
+        preBuildCleanup()
+    }
+    label('steeltoe')
+    scm {
+        git {
+            remote {
+                github('Steeltoe/Samples', 'https')
+                branch(branchName)
+            }
+        }
+    }
+    steps {
+        shell('echo HI')
+    }
+    publishers {
+            mailer('ccheetham@pivotal.io', true, false
+    }
+    logRotator {
+        numToKeep 5
+    }
+}
+
+// vim: et sw=4 sts=4
