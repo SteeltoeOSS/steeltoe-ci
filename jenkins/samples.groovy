df9e3fe413f2f3f3e554dad76ee765df7f213da0
Chris Cheetham <chris@cheetham.com>
2018-03-29 11:59:11 -0400

    Dump env

--- jenkins/samples.groovy
+++ jenkins/samples.groovy
@@ -9,6 +9,7 @@ recipients = [
 def psep = System.properties['path.separator']
 def path = "${System.env['JENKINS_HOME']}${psep}workspace${psep}${System.env['JOB_NAME']}"
 println "PWD ${new File(path)}"
+System.env.keySet().sort().each { println it }
 
 samplePaths = [
     'Connectors/src/AspDotNetCore/PostgreSql',
