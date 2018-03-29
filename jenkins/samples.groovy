61a0ceb6add1d727a9ab8bac6db7405d4ee54a5c
Chris Cheetham <chris@cheetham.com>
2018-03-29 12:36:26 -0400

    Tidy

--- jenkins/samples.groovy
+++ jenkins/samples.groovy
@@ -6,19 +6,6 @@ recipients = [
     'ccheetham',
 ]
 
-println "HOSTNAME ${'hostname'.execute().text}"
-
-// def projectDir = java.nio.file.Paths.get(System.env['JENKINS_HOME'], 'workspace', 'steeltoe-seed')
-def projectDir = java.nio.file.Paths.get(System.env['JENKINS_HOME'])
-println "listing $projectDir"
-projectDir.eachFile {
-    println it
-}
-
-// projectDir.resolve('features').eachLine {
-    // println "feature $it"
-// }
-
 samplePaths = [
     'Connectors/src/AspDotNetCore/PostgreSql',
     'Connectors/src/AspDotNetCore/PostgreEFCore',
