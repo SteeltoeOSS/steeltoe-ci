09395d776f613a45a215760b77df446794329ad6
Chris Cheetham <chris@cheetham.com>
2018-04-10 09:54:01 -0400

    Initial support for Windows

--- jenkins/samples.groovy
+++ jenkins/samples.groovy
@@ -2,76 +2,83 @@
  * Jenkins DSL for Steeltoe Samples
  */
 
-recipients = [
+samples = [
+    // 'Connectors/src/AspDotNetCore/PostgreSql',
+    // 'Connectors/src/AspDotNetCore/PostgreEFCore',
+    // 'Connectors/src/AspDotNetCore/Redis',
+    // 'Connectors/src/AspDotNetCore/MySqlEFCore',
+    // 'Connectors/src/AspDotNetCore/MySqlEF6',
+    // 'Connectors/src/AspDotNetCore/RabbitMQ',
+    // 'Connectors/src/AspDotNetCore/MySql/MySql',
+    // 'Configuration/src/AspDotNetCore/CloudFoundry',
+    'Configuration/src/AspDotNetCore/Simple',
+    // 'Configuration/src/AspDotNetCore/SimpleCloudFoundry',
+    // 'Security/src/CloudFoundrySingleSignon',
+    // 'Management/src/AspDotNetCore/CloudFoundry',
+]
+
+platforms = [
+    'ubuntu1604',
+    'win2012',
+]
+
+alertees = [
     'ccheetham',
 ]
 
-samplePaths = [
-    'Connectors/src/AspDotNetCore/PostgreSql',
-    'Connectors/src/AspDotNetCore/PostgreEFCore',
-    'Connectors/src/AspDotNetCore/Redis',
-    'Connectors/src/AspDotNetCore/MySqlEFCore',
-    'Connectors/src/AspDotNetCore/MySqlEF6',
-    'Connectors/src/AspDotNetCore/RabbitMQ',
-    'Connectors/src/AspDotNetCore/MySql/MySql',
-    'Configuration/src/AspDotNetCore/CloudFoundry',
-    'Configuration/src/AspDotNetCore/Simple',
-    'Configuration/src/AspDotNetCore/SimpleCloudFoundry',
-    'Security/src/CloudFoundrySingleSignon',
-    'Management/src/AspDotNetCore/CloudFoundry',
-]
-
-def sample2Job(def sample) {
-    "steeltoe-samples-${sample.split('/').findAll { !(it in ['src']) }.collect { it.toLowerCase() }.join('-')}"
+def jobForSample(def sample, def platform) {
+    "steeltoe-samples-${sample.split('/').findAll { !(it in ['src']) }.collect { it.toLowerCase() }.join('-')}-${platform}"
 }
 
-samplePaths.each { samplePath ->
-    job(sample2Job(samplePath)) {
-        wrappers {
-            credentialsBinding {
-                usernamePassword('STEELTOE_PCF_CREDENTIALS', 'steeltoe-pcf')
-            }
-            preBuildCleanup()
-        }
-        label('steeltoe')
-        scm {
-            git {
-                remote {
-                    github('SteeltoeOSS/Samples', 'https')
-                    branch('dev')
+samples.each { sample ->
+    platforms.each { platform ->
+        job(jobForSample(sample, platform)) {
+            wrappers {
+                credentialsBinding {
+                    usernamePassword('STEELTOE_PCF_CREDENTIALS', 'steeltoe-pcf')
                 }
+                preBuildCleanup()
             }
-        }
-        triggers {
-            scm('H/15 * * * *')
-        }
-        steps {
-            shell("ci/jenkins.sh ${samplePath}")
-        }
-        publishers {
-            archiveArtifacts('test.log')
-            mailer(recipients.collect { "${it}@pivotal.io" }.join(' '), true, false)
-            slackNotifier {
-                room('#steeltoe-pipelines')
-                notifyAborted(true)
-                notifyFailure(true)
-                notifyNotBuilt(true)
-                notifyUnstable(true)
-                notifyBackToNormal(true)
-                notifySuccess(false)
-                notifyRepeatedFailure(false)
-                startNotification(false)
-                includeTestSummary(false)
-                includeCustomMessage(false)
-                customMessage(null)
-                sendAs(null)
-                commitInfoChoice('AUTHORS_AND_TITLES')
-                teamDomain('pivotal')
-                authToken('MKRw5ybKEIgspDGHtyaq4TI2')
+            label('steeltoe', platform)
+                scm {
+                    git {
+                        remote {
+                            github('SteeltoeOSS/Samples', 'https')
+                                branch('dev')
+                        }
+                    }
+                }
+            triggers {
+                scm('H/15 * * * *')
             }
-        }
-        logRotator {
-            numToKeep(5)
+            steps {
+                shell("ci/jenkins.${platform.startsWith('win') ? 'bat' : 'sh'} ${sample}")
+            }
+            publishers {
+                archiveArtifacts('test.log')
+                    mailer(alertees.collect { "${it}@pivotal.io" }.join(' '), true, false)
+                    // slackNotifier {
+                    //     room('#steeltoe-pipelines')
+                    //     notifyAborted(true)
+                    //     notifyFailure(true)
+                    //     notifyNotBuilt(true)
+                    //     notifyUnstable(true)
+                    //     notifyBackToNormal(true)
+                    //     notifySuccess(false)
+                    //     notifyRepeatedFailure(false)
+                    //     startNotification(false)
+                    //     includeTestSummary(false)
+                    //     includeCustomMessage(false)
+                    //     customMessage(null)
+                    //     sendAs(null)
+                    //     commitInfoChoice('AUTHORS_AND_TITLES')
+                    //     teamDomain('pivotal')
+                    //     authToken('MKRw5ybKEIgspDGHtyaq4TI2')
+                    // }
+            }
+            logRotator {
+                numToKeep(5)
+            }
         }
     }
 }
