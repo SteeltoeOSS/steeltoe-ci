0aed99c1d96bb180b48339c38b7eee7176803ef1
Chris Cheetham <chris@cheetham.com>
2018-08-13 17:24:12 -0400

    disabling jobs until account setup for pcfone

--- jenkins/samples.groovy
+++ jenkins/samples.groovy
@@ -36,7 +36,7 @@ def jobForSample(def sample, def platform) {
 samplePaths.each { samplePath ->
     platforms.each { platform ->
         job(jobForSample(samplePath, platform)) {
-            // disabled()
+            disabled()
             wrappers {
                 credentialsBinding {
                     usernamePassword('STEELTOE_PCF_CREDENTIALS', 'steeltoe-pcf')
