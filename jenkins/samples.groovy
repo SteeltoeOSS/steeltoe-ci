08967fca3c9e9b8f376b1ad255cba7ad7952f994
Chris Cheetham <chris@cheetham.com>
2019-02-15 15:35:29 -0500

    disable windows builds until windows node back online

--- jenkins/samples.groovy
+++ jenkins/samples.groovy
@@ -36,7 +36,9 @@ def jobForSample(def sample, def platform) {
 samplePaths.each { samplePath ->
     platforms.each { platform ->
         job(jobForSample(samplePath, platform)) {
-            // disabled()
+            if (platform == 'win2012') {
+                disabled()
+            }
             wrappers {
                 credentialsBinding {
                     usernamePassword('STEELTOE_PCF_CREDENTIALS', 'steeltoe-pcf')
